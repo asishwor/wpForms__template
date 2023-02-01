644da4cb6dad76ace2b5a38248635205a4f50cc7
ishwor <asishwor@gmail.com>
2023-02-01 12:18:24 +0545

    completed

---
+++ main.js
@@ -0,0 +1,27 @@
+import "./css/style.css";
+import "./css/media.css";
+import { setupCounter } from "./counter.js";
+
+// video play function
+const bannerVideoPlay = () => {
+  const video = document.querySelector(".banner__video video");
+  const playBtn = document.querySelector(".banner__play-btn .fa-solid");
+  playBtn.addEventListener("click", (e) => {
+    const playBtnClass = e.target.classList;
+    if (playBtnClass.contains("fa-play") && !video.ended) {
+      playBtnClass.replace("fa-play", "fa-pause");
+      video.play();
+      video.parentNode.parentNode.classList.add("active");
+      video.controls = true;
+    } else {
+      playBtnClass.replace("fa-pause", "fa-play");
+      video.controls = false;
+      video.pause();
+      video.parentNode.parentNode.classList.remove("active");
+
+    }
+  });
+};
+console.log("first");
+bannerVideoPlay();
+setupCounter(document.querySelector("#counter"));
